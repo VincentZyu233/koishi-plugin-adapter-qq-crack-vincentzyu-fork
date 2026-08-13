8bec3faaa6382d0555ef05d0285615eb03a97dd4
VincentZyu233 <1830540513zyu@gmail.com>
2026-08-13 20:44:36 +0800

    feat(console): 添加指令面板侧边栏图标

---
+++ client/icons/command-panel.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    class="k-icon"
+    viewBox="2.5 2.5 19 19"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1.8"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M5.25 5.25h13.5a2 2 0 012 2v8.5a2 2 0 01-2 2h-7.2l-4.3 3v-3h-2a2 2 0 01-2-2v-8.5a2 2 0 012-2z" />
+    <path d="M7 9l1.7 1.5L7 12" />
+    <path d="M11.25 12h3.25" />
+    <path d="M11.25 8.75h4.75" />
+  </svg>
+</template>
--- client/index.ts
+++ client/index.ts
@@ -1,12 +1,16 @@
-import type { Context } from '@koishijs/client'
+import { Context, icons } from '@koishijs/client'
 import PanelSettings from './settings.vue'
+import CommandPanelIcon from './icons/command-panel.vue'
 
 export default (ctx: Context) => {
+  icons.register('activity:qq-command-panel', CommandPanelIcon)
+
   ctx.page({
+    id: 'qq-command-panel',
     name: 'QQ 指令面板',
     path: '/qq-command-panel',
     component: PanelSettings,
-    icon: 'list',
+    icon: 'activity:qq-command-panel',
     order: 420,
   })
 }
